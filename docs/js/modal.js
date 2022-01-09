1b1981c7a0f2d12e0ce8f8fa69089e3fd861cafc
YZPhoto <93886380+YZPhoto@users.noreply.github.com>
2022-01-10 00:33:01 +0200

    Update modal.js

--- docs/js/modal.js
+++ docs/js/modal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (() => {
     const refs = {
         openModalBtn: document.querySelector('[data-modal-open]'),
@@ -12,19 +11,4 @@
     function toggleModal() {
         refs.modal.classList.toggle('is-hidden');
     }
-=======
-(() => {
-    const refs = {
-        openModalBtn: document.querySelector('[data-modal-open]'),
-        closeModalBtn: document.querySelector('[data-modal-close]'),
-        modal: document.querySelector('[data-modal]'),
-    };
-
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-
-    function toggleModal() {
-        refs.modal.classList.toggle('is-hidden');
-    }
->>>>>>> 7b612e7df37a78722aa8c1e1335f95f7510ede48
 })();
